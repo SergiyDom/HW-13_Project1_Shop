774c69ec9eed442431c290bd2ff7555144fe5759
Sergiy D <SergiyDom@users.noreply.github.com>
2020-08-01 00:30:53 +0300

    add DB Orders class

---
+++ src/main/java/com/domaranskiy/database/Orders.java
@@ -0,0 +1,21 @@
+package com.domaranskiy.database;
+
+import com.domaranskiy.entities.models.order.Order;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Orders {
+    private static Orders instance;
+    private static List<Order> orderList = new ArrayList<>();
+
+    public Orders() {
+    }
+
+    public static Orders getInstance() {
+        if (instance == null){
+            instance = new Orders();
+        }
+        return instance;
+    }
+}
