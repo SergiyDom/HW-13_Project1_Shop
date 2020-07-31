62e1138a1d7d418ca64ecd950ba84cf2232c847a
Sergiy D <SergiyDom@users.noreply.github.com>
2020-08-01 00:04:53 +0300

    add user, product and order classes

---
+++ src/main/java/com/domaranskiy/entities/models/user/Roles.java
@@ -0,0 +1,15 @@
+package com.domaranskiy.entities.models.user;
+
+public enum Roles {
+    ADMIN(1),
+    USER(0);
+    int role;
+
+    Roles(int role) {
+        this.role = role;
+    }
+
+    public int getRole() {
+        return role;
+    }
+}
