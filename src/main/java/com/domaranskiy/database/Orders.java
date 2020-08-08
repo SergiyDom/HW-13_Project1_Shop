788cce5213ddd45cecb0e90b54aa7e9c376c11a1
Sergiy D <SergiyDom@users.noreply.github.com>
2020-08-08 10:52:32 +0300

    added functional addOrder

--- src/main/java/com/domaranskiy/database/Orders.java
+++ src/main/java/com/domaranskiy/database/Orders.java
@@ -1,7 +1,9 @@
 package com.domaranskiy.database;
 
 import com.domaranskiy.models.order.Order;
+import com.domaranskiy.models.order.OrderEditStatus;
 import com.domaranskiy.models.order.OrderStatus;
+import com.domaranskiy.models.product.ProductTypes;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,11 +16,12 @@ public class Orders {
     }
 
     public static synchronized Orders getInstance() {
-        if (instance == null){
+        if (instance == null) {
             instance = new Orders();
         }
         return instance;
     }
+
     public static ArrayList<Order> getOrdersInCartByUser(String userName) {
         ArrayList<Order> ordersInCart = new ArrayList<>();
         for (Order order : orderList) {
@@ -28,6 +31,7 @@ public class Orders {
         }
         return ordersInCart;
     }
+
     public static ArrayList<Order> getClosedOrdersByUser(String userName) {
         ArrayList<Order> ordersInCart = new ArrayList<>();
         for (Order order : orderList) {
@@ -36,5 +40,40 @@ public class Orders {
             }
         }
         return ordersInCart;
+    }
+
+    public void addOrder(String userName, String productName, ProductTypes productTypes, int quantity) throws Exception {
+        if (checkIfActiveOrderExists(userName, productName, productTypes)) {
+            Order order = getOrder(userName, productName, productTypes);
+            order.setQuantity(order.getQuantity() + quantity);
+        }else {
+            orderList.add(new Order(userName, productName, productTypes, quantity, OrderStatus.IN_CART, OrderEditStatus.EDITABLE));
+        }
+    }
+
+    private boolean checkIfActiveOrderExists(String userName, String productName, ProductTypes productTypes) {
+        for (Order order : orderList) {
+            if (order.getUserName().equals(userName)
+                    && order.getProductName().equals(productName)
+                    && order.getProductTypes().equals(productTypes)
+                    && order.getOrderStatus().equals(OrderStatus.IN_CART)
+            ) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private Order getOrder(String userName, String productName, ProductTypes productTypes) throws Exception {
+        for (Order order : orderList) {
+            if (order.getUserName().equals(userName)
+                    && order.getProductName().equals(productName)
+                    && order.getProductTypes().equals(productTypes)
+                    && order.getOrderStatus().equals(OrderStatus.IN_CART)
+            ) {
+                return order;
+            }
+        }
+        throw new Exception("Order not found");
     }
 }
